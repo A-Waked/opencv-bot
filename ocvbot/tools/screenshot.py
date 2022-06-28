4289345f2d193def393885ce9322ec37a4621318
Abdul <abdulw.3000@gmail.com>
2022-06-28 18:28:56 -0400

    Added training key and example output after training

--- ocvbot/tools/screenshot.py
+++ ocvbot/tools/screenshot.py
@@ -1,10 +1,18 @@
 import argparse
 import cv2 as cv
 import os
+import subprocess
+import re
 from time import time
 from ocvbot.botlib.windowcapture import WindowCapture
 from ocvbot.botlib.vision import Vision
 from ocvbot.botlib.hsvfilter import HSVFilter
+
+'''
+Allows you to view the capture window and obtain training data for cascade classifier.
+Additionally allows you to view the matched templates given and apply/save new filters to 
+the needle_filter.txt file.
+'''
 
 def run(path, needle):
     path = path.replace("\\", "/")  # fix path for windows
@@ -93,8 +101,8 @@ def run(path, needle):
             hsvfilter = get_filter(path)
             vision.set_trackbar_positions(hsvfilter)
 
-        # t - toggle template matching
-        elif key == ord('t'):
+        # m - toggle template matching
+        elif key == ord('m'):
             # toggle template matching
             if needle:
                 matching = not matching
@@ -138,6 +146,12 @@ def run(path, needle):
             if output_size > 0.2:
                 output_size -= 0.1
 
+        # 't' - generate required files for training, and start training
+        elif key == ord('t'):
+            cv.destroyAllWindows()
+            start_training(path)
+            break
+
         # automaticallly save a screenshot
         if toggle == True:
             if spsc == sps:
@@ -150,8 +164,6 @@ def run(path, needle):
                 print("{}. Saved: {}negative-{}.jpg".format(neg,path,loop_time))
                 spsc = 0
             spsc += 1
-
-        
 
 def get_filter(path):
     hsvfilter = HSVFilter()
@@ -186,3 +198,60 @@ def save_filter(path, hsvfilter):
         f.write(str(hsvfilter.vAdd)+"\n")
         f.write(str(hsvfilter.vSub)+"\n")
     print("Filter values saved to needle_filter.txt")
+
+# save the positive and negative images paths to a text file, manually draw rectangles, generate vector files then begin training 
+def start_training(path):
+    # save the name of every file in the folder 'negative' to a text file
+    # for positive training data use opencv_annotation program for now
+    # TODO implement own solution for positive files
+    with open(path+"negative_paths.txt", "w") as f:
+        for file in os.listdir(path+"negative"):
+            f.write("negative/" + file + "\n")
+    print("Saved negative paths to negative_paths.txt")
+
+    # Run the opencv_annotation program to generate positive training data text file
+    # TODO allow user to input path
+    tools_path = "C:/opencv/build/x64/vc15/bin/"
+    save_path = path+"positive_paths.txt"
+    positive_path = path+"positive/"
+    box_count = 0
+
+    if os.path.exists(tools_path):
+        # subprocess.call([tools_path + "opencv_annotation.exe", "--annotations=" + save_path, "--images=" + positive_path])
+
+        # replace all '\' in the generated text file with '/'
+        with open(save_path, "r") as f:
+            lines = f.readlines()
+
+        with open(save_path, "w") as f:
+            for line in lines:
+                # count the number of boxes per image by matching everything after .jpg
+                # TODO change this to match the first number as that's the box count of the image
+                p1 = 'jpg (.*)'
+                p2 = '\d+'
+                s = re.findall(p1, line)
+                box_count += (len(re.findall(p2, s[0])) - 1) / 4
+
+                f.write(line.replace(path, '').replace('\\', '/'))
+        
+        print("Saved positive paths + match boxes to positive_paths.txt. Box count: {}".format(box_count))
+
+        subprocess.call([tools_path + "opencv_createsamples.exe", "-info", save_path, "-num", str(box_count + 1), "-w", "24", "-h", "24", "-vec", path+"positive_samples.vec"])
+
+        # create a directory called cascade
+        if not os.path.exists(path+"cascade"):
+            os.makedirs(path+"cascade")
+        
+
+        print(tools_path + "opencv_traincascade.exe" + " -data " + "\""+path+"cascade/"+"\"" + " -vec " + "\""+path+"positive_samples.vec"+"\""+ " -bg "+ "\""+path+"negative_paths.txt"+"\""+ " -numPos "+ str(int(box_count * 0.8))+ " -numNeg "+ str(int(box_count * 0.8 * 2))+ " -numStages "+ "10"+ " -w "+ "24"+ " -h ", "24")
+
+        # add " as arguments dont handle spaces the same way as the other applications
+        # Only works when called from script directory for some reason. Hardcoding the full path doesn't work
+        subprocess.call([tools_path + "opencv_traincascade.exe", "-data", "cascade/", "-vec", "positive_samples.vec", "-bg","negative_paths.txt", "-numPos", str(int(box_count * 0.8)), "-numNeg", str(int(box_count * 0.8 * 2)), "-numStages", "10", "-w", "24", "-h", "24"], cwd=path)
+
+    else:
+        print("opencv tools path does not exist in the specified path: " + tools_path)
+        print("Make sure its located there or manually run it to generate the positive_paths.txt file")
+
+    
+    
